dbe2eb543308e8e69fad2acfbd3e9bad672b53df
JoYunsik <choys0528@naver.com>
2023-07-08 16:41:43 +0900

    djust Django course 8 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 
+from django.shortcuts import render
 
-# from django.shortcuts import render
 
 # Create your views here.
 def hello_world(request):
-    return HttpResponse('hello world')
+    return render(request, 'base.html')
